8c80795ff006e7e00ca4ee6be1a578f73c7b7d5c
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-10-19 22:06:22 +0100

    added task description

--- OptimusPrime.java
+++ OptimusPrime.java
@@ -1,5 +1,11 @@
-package day2;
+/*
+Write a program that reads an integer number from the user, and then outputs the closest prime number. If there
+are two prime numbers at the same distance, it should output both. For instance, if the user enters 5116, the output
+should be 5113 and 5119.
 
+*/
+
+package day2;
 
 import java.util.Scanner;
 
